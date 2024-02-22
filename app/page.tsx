7df6a432ad68587b37b98edac74a388ee51e4ed5
Jamin Chan <jaminchan321@gmail.com>
2024-02-22 00:08:38 -0800

    finished the education portion

--- app/page.tsx
+++ app/page.tsx
@@ -67,22 +67,59 @@ export default function Home() {
               Education
           </p>
         </div>
-        <div className="grid grid-cols-3 gap-10 w-9/10 h-80">
-          <div className="rounded-lg bg-gradient-to-r from-red-900 to-yellow-400">
-            <p className="text-3xl font-semibold ">
+        <div className="grid grid-cols-3 gap-10 w-11/12 h-min text-black text-wrap">
+          <div className="rounded-lg bg-gradient-to-r from-pink-600 via-pink-500 to-fuchsia-500">
+            <p className="m-2 text-center text-3xl font-semibold ">
                 Mills High School
             </p>
+            <p className="text-center text-2xl">
+                2017-2021
+            </p>
+            <ul className="flex justify-center flex-wrap text-xl mt-4 *:p-1 *:m-1 *:bg-white *:rounded-md *:border-2 *:border-black *:border-solid hover:*:border-amber-500">
+              <li>AP Calculus</li>
+              <li>AP Chemistry</li>
+              <li>AP Statistics</li>
+              <li>AP Computer Science A</li>
+              <li>AP Physics</li>
+            </ul>
           </div>
-          <div className="rounded-lg bg-gradient-to-r from-red-900 via-yellow-400 to-white">
-            <p className="text-3xl font-semibold ">
+          <div className="rounded-lg bg-fuchsia-500">
+            <p className="m-2 text-center text-3xl font-semibold ">
                 Skyline College
             </p>
+            <p className="text-center text-2xl">
+                2021-2023
+            </p>
+            <ul className="flex justify-center flex-wrap text-xl mt-4 *:p-1 *:m-1 *:bg-white *:rounded-md *:border-2 *:border-black *:border-solid hover:*:border-amber-500">
+              <li>Calculus 1</li>
+              <li>Calculus 2</li>
+              <li>Calculus 3</li>
+              <li>Intro Object-Oriented Prog:C++</li>
+              <li>Data Structures C++</li>
+              <li>Microcontrollers with C++</li>
+              <li>Computer Arch. & Assembly Lang</li>
+              <li>Linear Algebra</li>
+              <li>Discrete Mathematics</li>
+              <li>Ordinary Differential Equations</li>
+            </ul>
           </div>
-          <div className="rounded-lg bg-gradient-to-r from-blue-900 to-yellow-400">
-            <p className="text-3xl font-semibold ">
+          <div className="rounded-lg bg-gradient-to-r from-fuchsia-500 via-purple-500 to-purple-600">
+            <p className="m-2 text-center text-3xl font-semibold ">
                 University of California, Davis
             </p>
+            <p className="text-center text-2xl">
+                2023-2025
+            </p>
+            <ul className="flex justify-center flex-wrap text-xl mt-4 *:p-1 *:m-1 *:bg-white *:rounded-md *:border-2 *:border-black *:border-solid hover:*:border-amber-500">
+              <li>Computer Architecture</li>
+              <li>Software Development & Object-Oriented Programing in C++</li>
+            </ul>
           </div>
+        </div>
+        <div className="grid grid-cols-1 place-items-center h-fit pt-32">
+            <p className="text-6xl font-semibold">
+              Experience
+            </p>
         </div>
         
       </div>

c2fe4193789eb0e264fe4105e9516518748a8579
Jamin Chan <jaminchan321@gmail.com>
2024-03-02 01:01:08 -0800

    added some more to education

--- app/page.tsx
+++ app/page.tsx
@@ -61,21 +61,21 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 place-items-center h-fit">
+      <div className="flex flex-col place-items-center h-dvh">
         <div className="m-8">
-          <p className="text-6xl font-semibold">
+            <p className="text-6xl font-semibold">
               Education
-          </p>
+            </p>
         </div>
         <div className="grid grid-cols-3 gap-10 w-11/12 h-min text-black text-wrap">
           <div className="rounded-lg bg-gradient-to-r from-pink-600 via-pink-500 to-fuchsia-500">
-            <p className="m-2 text-center text-3xl font-semibold ">
+            <p className="m-2 text-center text-3xl font-semibold">
                 Mills High School
             </p>
             <p className="text-center text-2xl">
                 2017-2021
             </p>
-            <ul className="flex justify-center flex-wrap text-xl mt-4 *:p-1 *:m-1 *:bg-white *:rounded-md *:border-2 *:border-black *:border-solid hover:*:border-amber-500">
+            <ul className="flex justify-center flex-wrap text-xl mt-4 *:p-1 *:m-1 *:bg-white *:rounded-md *:border-2 *:border-black *:border-solid hover:*:border-amber-500 hover:*:bg-slate-200">
               <li>AP Calculus</li>
               <li>AP Chemistry</li>
               <li>AP Statistics</li>
@@ -90,7 +90,7 @@ export default function Home() {
             <p className="text-center text-2xl">
                 2021-2023
             </p>
-            <ul className="flex justify-center flex-wrap text-xl mt-4 *:p-1 *:m-1 *:bg-white *:rounded-md *:border-2 *:border-black *:border-solid hover:*:border-amber-500">
+            <ul className="flex justify-center flex-wrap text-xl mt-4 *:p-1 *:m-1 *:bg-white *:rounded-md *:border-2 *:border-black *:border-solid hover:*:border-amber-500 hover:*:bg-slate-200">
               <li>Calculus 1</li>
               <li>Calculus 2</li>
               <li>Calculus 3</li>
@@ -110,18 +110,17 @@ export default function Home() {
             <p className="text-center text-2xl">
                 2023-2025
             </p>
-            <ul className="flex justify-center flex-wrap text-xl mt-4 *:p-1 *:m-1 *:bg-white *:rounded-md *:border-2 *:border-black *:border-solid hover:*:border-amber-500">
+            <ul className="flex justify-center flex-wrap text-xl mt-4 *:p-1 *:m-1 *:bg-white *:rounded-md *:border-2 *:border-black *:border-solid hover:*:border-amber-500 hover:*:bg-slate-200">
               <li>Computer Architecture</li>
               <li>Software Development & Object-Oriented Programing in C++</li>
             </ul>
           </div>
-        </div>
-        <div className="grid grid-cols-1 place-items-center h-fit pt-32">
+        </div>  
+      </div>
+      <div className="flex flex-col place-items-center h-screen m-8">
             <p className="text-6xl font-semibold">
-              Experience
+              Projects
             </p>
-        </div>
-        
       </div>
 
 

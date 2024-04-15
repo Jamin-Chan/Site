22d29fd2ba7206db46bb45419daec27405434767
Jamin Chan <jaminchan321@gmail.com>
2024-04-14 23:59:09 -0700

    added all the imgaes and uypdated resume

--- public/pages/frontPage.tsx
+++ public/pages/frontPage.tsx
@@ -13,7 +13,7 @@ export const FrontPage: React.FC= () => {
                     <button className="transition hover:-translate-y-2">
                         <Image
                             className="rounded shadow-lg bg-transparent mr-4"
-                            src="https://cdn.discordapp.com/attachments/1207939238163910736/1207939307709669376/2048px-LinkedIn_icon.png?ex=65e17825&is=65cf0325&hm=12d9b49dddd2c8f01844f6310b46cac11e40a2cc71ba6a43a63600e2ed7f7458&"
+                            src="\site\assets\linkedinLogo.png"
                             alt="LinkedIn Logo"
                             width={50}
                             height={50}
@@ -24,7 +24,7 @@ export const FrontPage: React.FC= () => {
                     <button className="transition hover:-translate-y-2">
                         <Image
                             className="rounded shadow-lg bg-transparent mr-4"
-                            src="https://cdn.discordapp.com/attachments/1112541604318822600/1113948933572534314/174855.png?ex=65e0fd49&is=65ce8849&hm=e0c4c9a54b02e179384c0e075406858f1e03d95bab3157ecbf2c40fb753fcf1a&"
+                            src="\site\assets\instagramLogo.png"
                             alt="Instagram Logo"
                             width={50}
                             height={50}
@@ -35,7 +35,7 @@ export const FrontPage: React.FC= () => {
                     <button className="transition hover:-translate-y-2">
                         <Image
                             className="rounded shadow-lg bg-transparent mr-4"
-                            src="https://cdn.discordapp.com/attachments/1112541604318822600/1113947838007754752/github-mark.png?ex=65e0fc44&is=65ce8744&hm=9fea9a20980c29fdcb70b26e9fabf89955ce0bad19dcb0e7928b64e8b69166b0&"
+                            src="\site\assets\GithubLogo.png"
                             alt="Github Logo"
                             width={50}
                             height={50}
@@ -46,7 +46,7 @@ export const FrontPage: React.FC= () => {
                     <button className="transition hover:-translate-y-2">
                         <Image
                             className="rounded shadow-lg bg-transparent mr-4"
-                            src="https://cdn.discordapp.com/attachments/1207939238163910736/1208003151060279296/768px-Mail_28iOS29.png?ex=65e1b39a&is=65cf3e9a&hm=4a2deff000ef4c85e37b7cd7275f280da8abfc6ed684fda6c34606714e495159&&"
+                            src="\site\assets\mailLogo.png"
                             alt="Github Logo"
                             width={50}
                             height={50}
